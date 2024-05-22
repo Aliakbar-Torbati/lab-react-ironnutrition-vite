94ff93a012a684b924b52605a35b351f9d4afdfc
Aliakbar Torbati <alitorbati1368@gmail.com>
2024-05-22 22:20:21 +0200

    form added

--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,58 @@
-// Your code here
+import { useState } from "react"
+
+
+const AddFoodForm = ({ allFood, setAllFood, handleCreateFood, Name, Image, Calories, Servings, setName }) => {
+    // const [Name, setName]= useState ("")
+    // const [Image, setImage]= useState ("")
+    // const [Calories, setCalories]= useState (0)
+    // const [Servings , setServings ]= useState (0)
+
+    // const handleCreateFood= (event)=>{
+    //     event.preventDefault();
+    //     const newFood={id: Date.now(), Name, Image, Calories, Servings};
+    //     console.log("new food",newFood)
+    //     setAllFood([newFood, ...allFood]);
+    // }
+
+  return (
+    <div>
+        {/* <form onSubmit={handleCreateFood}>
+            <span>Add Food Entry</span>
+            <div>
+            <label>
+                Name
+                <input name="Name" type="text" placeholder="Name" value={Name} onChange={(event) => {
+                setName(event.target.value)
+                }}
+                />
+            </label>
+    
+         <label>
+                Image
+                <input name="image" type="url" placeholder="Food Image" value={Image} onChange={(event) => {
+                setImage(event.target.value)
+                }} />
+            </label>
+    
+            <label>
+            Calories
+                <input name="Calories" type="number" placeholder="Calories" value={Calories} onChange={(event) => {
+                setCalories(event.target.value)
+                }} />
+            </label>
+    
+            <label>
+            Servings
+                <input name="Servings" type="number" placeholder="Servings" value={Servings} onChange={(event) => {
+                setServings(event.target.value)
+                }}/>
+            </label> 
+            </div>
+    
+            <button type="submit">Add Food</button>
+        </form> */}
+    </div>
+  )
+}
+
+export default AddFoodForm
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,33 @@
-// Your code here
+import "../App.css";
+
+const FoodBox = ({ allFood, setAllFood }) => {
+    const handleDelete = (foodId)=>{
+        const filteredFood= allFood.filter((food)=>{
+            return food.id !== foodId 
+        })
+        setAllFood(filteredFood)
+    }
+  return (
+    <div>
+      {allFood.map((food) => {
+        return (
+          <div key={food.id} className="foodbox">
+               
+            <p>{food.name}</p>
+
+            <img src={food.image} />
+
+            <p>Calories: {food.calories}</p>
+            <p>Servings {food.servings}</p>
+            <p>
+              <b>Total Calories: {food.calories*food.servings} </b> kcal
+            </p>
+            <button onClick={()=>handleDelete(food.id)}>Delete</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FoodBox;
